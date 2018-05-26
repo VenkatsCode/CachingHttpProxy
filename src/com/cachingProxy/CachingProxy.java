67fedef146d6266eae0413757fcb9b540e835d18
VenkatsCode <vreddy2941990@gmail.com>
2018-05-26 12:00:07 -0400

    main class with client socket enabled on port 3128

---
+++ src/com/cachingProxy/CachingProxy.java
@@ -0,0 +1,56 @@
+package com.cachingProxy;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CachingProxy {
+
+	private ServerSocket serverSocket;
+	private volatile boolean running = true;
+	static HashMap<String, File> cacheData;
+	static ArrayList<Thread> requestsInProgress;
+	
+	public CachingProxy(int port) {
+		cacheData = new HashMap<>();
+		requestsInProgress = new ArrayList<>();
+
+		try {
+			serverSocket = new ServerSocket(port);
+			System.out.println("Waiting on port " + serverSocket.getLocalPort() + "..");
+			running = true;
+		} 
+		catch (SocketException se) {
+			se.printStackTrace();
+		}
+		catch (SocketTimeoutException ste) {
+			ste.printStackTrace();
+		} 
+		catch (IOException io) {
+			io.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		CachingProxy cahicngProxy = new CachingProxy(3128);
+		cahicngProxy.openSocket();
+	}
+	
+	public void openSocket(){
+		while(running){
+			try {
+				Socket socket = serverSocket.accept();
+			} catch (SocketException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
